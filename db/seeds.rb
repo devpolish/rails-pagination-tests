78daca4c547d79093c0aa613ecc1300935420433
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2019-01-15 09:51:40 -0500

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,11 @@
 # frozen_string_literal: true
 
+5000.times do
+  Item.create(
+    name: Faker::Food.dish,
+    price: rand(2..100).to_f,
+    main_ingredient: Faker::Food.ingredient,
+    invoice_reference: Faker::Invoice.unique.reference
+  )
+end
 
