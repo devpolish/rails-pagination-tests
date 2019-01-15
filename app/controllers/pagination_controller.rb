6a2fd5cdf78f42cdd33e9c24e38bbc7af061f7f2
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2019-01-15 09:41:15 -0500

    add namespacing

---
+++ app/controllers/pagination_controller.rb
@@ -0,0 +1,2 @@
+class PaginationController < ApplicationController
+end
---
+++ test/controllers/pagination_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PaginationControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
